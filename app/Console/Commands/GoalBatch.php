ea3b8d45969cda4b70804f66e17237deaf6969f8
wanosukecake <wanosukecake@gmail.com>
2021-02-13 13:54:57 +0900

    circleCI対応追加

--- app/Console/Commands/GoalBatch.php
+++ app/Console/Commands/GoalBatch.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use App\Services\GoalService;
+use Illuminate\Support\Facades\Log;
 
 class GoalBatch extends Command
 {
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,5 +36,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        // 本番環境(Heroku)でhttpsを強制する
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
