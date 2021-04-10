15ff84f5b04447658bcbc1d0afe0017d14cbf16a
wanosukecake <wanosukecake@gmail.com>
2021-04-10 18:32:04 +0900

    ログイン後に/homeに遷移してしまう件対応

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,6 +19,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
+    public const DASHBOARD = '/admin/dashboard';
+
     /**
      * The controller namespace for the application.
      *
